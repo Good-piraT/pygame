10040848ab1354d5b28ac43352037da0e5b2f1a2
annasargayeva <4538067-annasargayeva@users.noreply.replit.com>
2024-01-10 11:28:44 +0000

    new game + ones

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 import sys, os
 
 
-class Board: # класс, реализующий игровое поле
+class Board:  # класс, реализующий игровое поле
 
   def __init__(self, width, height):
     self.width = width
@@ -13,21 +13,33 @@ class Board: # класс, реализующий игровое поле
     self.top = 10
     self.cell_size = 30
 
-  def set_view(self, left, top, cell_size): #изменение расположение правого верхнего угла и размера клеток
+  def set_view(
+      self, left, top, cell_size
+  ):  #изменение расположение правого верхнего угла и размера клеток
     self.left = left
     self.top = top
     self.cell_size = cell_size
 
-  def render(self, screen, color='black'): #
+  def render(self, screen, color='black'):  #рендер
     for x in range(self.width):
       for y in range(self.height):
         elem = self.board[y][x]
-        pygame.draw.rect(screen, color,
-                         (self.left + self.cell_size * x, self.top +
-                          self.cell_size * y, self.cell_size, self.cell_size),
-                         2)
+        if elem == 0:
+          pygame.draw.rect(
+              screen, 'black',
+              (self.left + self.cell_size * x, self.top + self.cell_size * y,
+               self.cell_size, self.cell_size), 2)
+        else:
+          if elem == 1:
+            color = 'red'
+          elif elem == 2:
+            color = 'orange'
+          pygame.draw.rect(
+              screen, color,
+              (self.left + self.cell_size * x, self.top + self.cell_size * y,
+               self.cell_size, self.cell_size))
 
-  def get_cell(self, mouse_pos): #дает координаты клетки при клике мышкой
+  def get_cell(self, mouse_pos):  #дает координаты клетки при клике мышкой
     mx = mouse_pos[0]
     my = mouse_pos[1]
     rx, ry = (self.left, self.top)
@@ -42,41 +54,79 @@ class Board: # класс, реализующий игровое поле
       cell_row = inner_my // self.cell_size
       return cell_row, cell_column
 
-  def on_click(self, cell_coords): #пишет координаты клеток в консоль
-    print(cell_coords)
+  def get_button(self, mouse_pos):
+    mouse_x = mouse_pos[0]
+    mouse_y = mouse_pos[1]
+    if mouse_x >= 87 and mouse_x <= 87 + 226 and mouse_y >= 423 and mouse_y <= 54 + 423:
+      return True
+    else:
+      return False
 
-  def get_click(self, mouse_pos): #связующий между get_cell и on_click
+  def button_act(self):
+    print("I'M BUTTON")
+    self.board = [[0] * self.width for _ in range(self.height)]
+
+  def on_click(self, cell_coords):  #пишет координаты клеток в консоль
+    print('board cell', cell_coords)
+
+  def get_click(self, mouse_pos):  #связующий между get_cell и on_click
+    is_button = self.get_button(mouse_pos)
     cell = self.get_cell(mouse_pos)
     self.on_click(cell)
+    if is_button:
+      self.button_act()
 
-  def move(self, direction): #делает ход, двигая клетки в указанном направлении
-    pass
-  
-  def ones(self): #добавляет еще одну единицу на доску в путом месте
+  def move(self,
+           direction):  #делает ход, двигая клетки в указанном направлении
+    if direction == 'right':
+      for row in range(4):
+        pass
+    self.ones()
+
+  def ones(self):  #добавляет еще одну единицу на доску в путом месте
+    br = False
     for row in range(4):
       x = randint(0, 1)
       if 0 in self.board[row] and x == 1:
         for elem in range(4):
-          num_zeros, index = zero_count(self.board[row])
-          q = randint(1, num_zeros)
-          print(row, index, num_zeros, q)
-          self.board[row][index[num_zeros]] = 1
+          num_zeros, index = zero_count(
+              self.board[row])  #количество нулей, тх индексы
+          q = randint(0, num_zeros - 1)
+          print('ноль оказалась q-тым', q, end=' ')
+          print('ряд', row, 'из индексов', index, 'из своих собратьев',
+                num_zeros)
+          self.board[row][index[q]] = 1
+          br = True
+          break
+        if br == True:
           break
 
 
-def zero_count(mass): #подсчет нулей в массиве
-  count = -1
+pygame.init()
+surf = pygame.display.set_mode((400, 500))
+pygame.display.set_caption('1024')
+
+
+def zero_count(mass):  #подсчет нулей в массиве
+  count = 0
   index = []
   for i in range(len(mass)):
     if mass[i] == 0:
       count += 1
       index.append(i)
-  return count, index #возвращает количество нулей и их индексы
+  return count, index  #возвращает количество нулей и их индексы
 
 
-pygame.init()
-surf = pygame.display.set_mode((400, 400))
-pygame.display.set_caption('1024')
+def draw_button(screen):
+  font = pygame.font.Font(None, 50)
+  text = font.render("NEW GAME!", True, 'black')
+  text_x = 200 - text.get_width() // 2
+  text_y = 450 - text.get_height() // 2
+  text_w = text.get_width()
+  text_h = text.get_height()
+  screen.blit(text, (text_x, text_y))
+  pygame.draw.rect(screen, 'black',
+                   (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
 
 
 def load_image(name, colorkey=None):
@@ -92,14 +142,8 @@ icon = load_image("icon.png")
 pygame.display.set_icon(icon)
 board = Board(4, 4)
 board.set_view(0, 0, 100)
+b_color = pygame.Color(240, 248, 255)
 running = True
-all_sprites = pygame.sprite.Group()
-sprite = pygame.sprite.Sprite()
-sprite.image = load_image("1.png")
-sprite.rect = sprite.image.get_rect()
-sprite.rect.x = 5
-sprite.rect.y = 20
-all_sprites.add(sprite)
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -110,15 +154,19 @@ while running:
       if event.key == pygame.K_ESCAPE:
         running = False
       elif event.key == pygame.K_UP:
+        print('up')
         board.move('up')
       elif event.key == pygame.K_DOWN:
+        print('down')
         board.move('down')
       elif event.key == pygame.K_RIGHT:
+        print('right')
         board.move('right')
       elif event.key == pygame.K_LEFT:
+        print('left')
         board.move('left')
-
-  surf.fill('white')
+  surf.fill(b_color)
+  draw_button(surf)
   board.render(surf)
   pygame.display.update()
 pygame.quit()
